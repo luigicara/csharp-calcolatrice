6632c8f10659d558afbd9208673a0445bb6e7c9f
luigicara <luigi.caravelli98@gmail.com>
2023-04-12 15:49:45 +0200

    complete

---
+++ Helper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal static class Helper
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static int Multi(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Multi(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static int Abs(int a)
+        {   
+            if (a < 0) 
+                return a + (a * -2);
+            return a;
+        }
+
+        public static double Abs(double a)
+        {
+            if (a < 0)
+                return a + (a * -2);
+            return a;
+        }
+
+        public static int Min(int a, int b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            return b;
+        }
+
+        public static double Min(double a, double b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            return b;
+        }
+
+        public static int Max(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            return b;
+        }
+
+        public static double Max(double a, double b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            return b;
+        }
+         
+        //bonus
+        public static double Expo(int _base, int power)
+        {   
+            if (power == 0)
+                return 1;
+
+            double baseDouble = _base;
+            double result = _base;
+            
+            if (power < 0 && _base != 0)
+            {
+                baseDouble = 1d / baseDouble;
+                result = baseDouble;
+            }
+
+            for (int i = 1; i < Helper.Abs(power); i++)
+                result *= baseDouble;
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main()
         {
-            
+            int a = -2;
+            int b = 3;
+
+            Console.WriteLine($"I numeri sono {a} e {b}");
+
+            Console.WriteLine($"La somma di questi due numeri è {Helper.Sum(a,b)}");
+
+            Console.WriteLine($"La sottrazione di questi due numeri è {Helper.Sub(a, b)}");
+
+            Console.WriteLine($"La moltiplicazione di questi due numeri è {Helper.Multi(a, b)}");
+
+            Console.WriteLine($"Il valore assoluto di {a} è {Helper.Abs(a)}");
+
+            Console.WriteLine($"Il minore tra questi due numeri è {Helper.Min(a, b)}");
+
+            Console.WriteLine($"Il maggiore tra questi due numeri è {Helper.Max(a, b)}");
+            //bonus
+            Console.WriteLine($"Il numero {a} elevato a {b} è uguale a {Helper.Expo(a, b)}");
+
+            double c = -1.2;
+            double d = 3.7;
+
+            Console.WriteLine($"\r\nI numeri sono {c} e {d}");
+
+            Console.WriteLine($"La somma di questi due numeri è {Helper.Sum(c, d)}");
+
+            Console.WriteLine($"La sottrazione di questi due numeri è {Helper.Sub(c, d)}");
+
+            Console.WriteLine($"La moltiplicazione di questi due numeri è {Helper.Multi(c, d)}");
+
+            Console.WriteLine($"Il valore assoluto di {c} è {Helper.Abs(c)}");
+
+            Console.WriteLine($"Il minore tra questi due numeri è {Helper.Min(c, d)}");
+
+            Console.WriteLine($"Il maggiore tra questi due numeri è {Helper.Max(c, d)}");
         }
     }
 }
